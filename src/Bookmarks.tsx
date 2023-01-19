04c7c936640086e9b217797e790309def55f0b8f
Misty Becker <mistyb01@uw.edu>
2023-01-18 20:11:36 -0800

    bookmarks.tsx conditionally renders based on category

--- src/Bookmarks.tsx
+++ src/Bookmarks.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
 interface Props {
   bookmarkData: {name: string, url: string, category: string}[];
 }
 
 // React.FC<Props> specifies that Bookmarks is a functional component.
-export const Bookmarks: React.FC<Props> = ({}) => {
+export const Bookmarks: React.FC<Props> = ({bookmarkData}) => {
+  const [category, setCategory] = useState('fun');
+
+  function handleCategoryChange(e: React.MouseEvent<HTMLButtonElement>) {
+    setCategory(e.currentTarget.id);
+  }
+
   return (
     <div className="bookmarks-container">
-        <ul className="bookmarks-list">
-          <li><a href="https://developer.mozilla.org/en-US/" target="_blank">mdn web docs</a></li>
-          </ul>
+      <div className='category-buttons'>
+        <button id="reference" onMouseEnter={handleCategoryChange}>reference</button>
+        <button id="courses" onMouseEnter={handleCategoryChange}>courses</button>
+        <button id="fun" onMouseEnter={handleCategoryChange}>fun</button>
+      </div>
+      <ul className="bookmarks-list">
+        {bookmarkData.map((bookmark) => {
+          if (bookmark.category === category) {
+            return (
+              <li>
+                <a href={bookmark.url} target="_blank">{bookmark.name}</a>
+              </li>
+            );
+          }
+        })}
+      </ul>
     </div>
   );
 }
