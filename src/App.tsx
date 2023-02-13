45f257e32e07dc31bd079341cf6c223ec15f82b1
Misty Becker <mistyb01@uw.edu>
2023-02-13 07:15:17 -0800

    moved static data to its own file

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,12 @@ import Bookmarks from './Bookmarks';
 import Categories from './Categories';
 import Tags from './Tags';
 import { Link } from './@types/app';
+import bookmarkData from './data/static-data';
+
 const App:FC = () => {
 
   
-  let bookmarkData: Link[] = [
-    {name: "mdn", url: "https://developer.mozilla.org/en-US/", description:"", category: "code", tag: "official docs"},
-    {name: "react", url: "https://reactjs.org/docs/hello-world.html", description:"",  category: "code", tag: "official docs"},
-    {name: "typescript", url: "https://www.typescriptlang.org/docs/", description:"", category: "code", tag: "official docs"},
-    {name: "css demystified", url: "https://courses.kevinpowell.co/view/courses/css-demystified", description:"", category: "code", tag: "online course"},
-    {name: "javascript dsa", url: "https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/", description:"", category: "code", tag: "online course"},
-    {name: "teach yourself cs", url: "https://teachyourselfcs.com/", description:"", category: "code", tag: "general guides"},
-    {name: "frontend roadmap", url: "https://roadmap.sh/frontend", description:"", category: "code", tag: "general guides"},
-    
-    {name: "yesterweb", url: "https://yesterweb.org/", description:"", category: "fun", tag: "indie web"},
-    {name: "sadgrl.online", url: "https://sadgrl.online/cyberspace/surf-the-web", description:"", category: "fun", tag: "indie web"},
-    {name: "rgb teahouse", url: "https://rgbteahouse.neocities.org/", description:"", category: "fun", tag: "indie web"},
-    {name: "vg museum", url: "http://www.vgmuseum.com/", description:"", category: "fun", tag: "video games"},
-    {name: "starmen.net", url: "http://www.starmen.net/", description:"", category: "fun", tag: "video games"},
-
-    {name: "loveariddle", url: "https://loveariddle.tumblr.com/", description:"", category: "art", tag: "web artists"},
-    {name: "emmmeralddd", url: "https://emmmeralddd.tumblr.com/", description:"", category: "art", tag: "web artists"},
-    {name: "mivvu", url: "https://mivvu-archive.tumblr.com/", description:"", category: "art", tag: "web artists"},
-    {name: "ovopack", url: "https://ovopack.tumblr.com/", description:"", category: "art", tag: "web artists"},
-    {name: "jiro sasumo", url: "https://www.pixiv.net/en/users/326389", description:"", category: "art", tag: "web artists"},
-    {name: "ohprcr", url: "https://ohprcr.tumblr.com", description:"", category: "art", tag: "web artists"},
-    {name: "worvies", url: "https://twitter.com/Worvies", description:"", category: "art", tag: "web artists"},
-    {name: "creamyghost", url: "https://twitter.com/CreamyGhost", description:"", category: "art", tag: "web artists"},
-    {name: "1041uuu", url: "https://1041uuu.tumblr.com", description:"", category: "art", tag: "web artists"},
-  ]
-
+  
   const [category, setCategory] = useState('code');
   const [tag, setTag] = useState('all');
   
@@ -59,8 +36,9 @@ const App:FC = () => {
       <header>
         <Categories {...props}/>
       </header>
-      <main>
+      <main className="horizontal-space">
         <div className="filter-menu">
+          <h3 className="filter-menu__header">filters</h3>
           <Tags {...props}/>
         </div>
         <div className="bookmarks">
---
+++ src/data/static-data.ts
@@ -0,0 +1,34 @@
+import { Link } from '../@types/app';
+
+const bookmarkData: Link[] = [
+    {name: "mdn", url: "https://developer.mozilla.org/en-US/", description:"", category: "code", tag: "official docs"},
+    {name: "react", url: "https://reactjs.org/docs/hello-world.html", description:"",  category: "code", tag: "official docs"},
+    {name: "typescript", url: "https://www.typescriptlang.org/docs/", description:"", category: "code", tag: "official docs"},
+    {name: "css demystified", url: "https://courses.kevinpowell.co/view/courses/css-demystified", description:"", category: "code", tag: "online course"},
+    {name: "javascript dsa", url: "https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/", description:"", category: "code", tag: "online course"},
+    {name: "teach yourself cs", url: "https://teachyourselfcs.com/", description:"", category: "code", tag: "general guides"},
+    {name: "frontend roadmap", url: "https://roadmap.sh/frontend", description:"", category: "code", tag: "general guides"},
+
+    {name: "loveariddle", url: "https://loveariddle.tumblr.com/", description:"", category: "art", tag: "web artists"},
+    {name: "emmmeralddd", url: "https://emmmeralddd.tumblr.com/", description:"", category: "art", tag: "web artists"},
+    {name: "mivvu", url: "https://mivvu-archive.tumblr.com/", description:"", category: "art", tag: "web artists"},
+    {name: "ovopack", url: "https://ovopack.tumblr.com/", description:"", category: "art", tag: "web artists"},
+    {name: "jiro sasumo", url: "https://www.pixiv.net/en/users/326389", description:"", category: "art", tag: "web artists"},
+    {name: "ohprcr", url: "https://ohprcr.tumblr.com", description:"", category: "art", tag: "web artists"},
+    {name: "worvies", url: "https://twitter.com/Worvies", description:"", category: "art", tag: "web artists"},
+    {name: "creamyghost", url: "https://twitter.com/CreamyGhost", description:"", category: "art", tag: "web artists"},
+    {name: "1041uuu", url: "https://1041uuu.tumblr.com", description:"", category: "art", tag: "web artists"},
+    {name: "みなはむ", url: "https://www.pixiv.net/en/users/228408", description:"", category: "art", tag: "web artists"},
+    {name: "pon-marsh", url: "https://www.pixiv.net/en/users/5093857", description:"", category: "art", tag: "web artists"},
+
+
+    {name: "yesterweb", url: "https://yesterweb.org/", description:"", category: "fun", tag: "indie web"},
+    {name: "sadgrl.online", url: "https://sadgrl.online/cyberspace/surf-the-web", description:"", category: "fun", tag: "indie web"},
+    {name: "rgb teahouse", url: "https://rgbteahouse.neocities.org/", description:"", category: "fun", tag: "indie web"},
+    {name: "vg museum", url: "http://www.vgmuseum.com/", description:"", category: "fun", tag: "video games"},
+    {name: "starmen.net", url: "http://www.starmen.net/", description:"", category: "fun", tag: "video games"},
+    {name: "sakugabooru", url: "http://www.sakugabooru.com/", description:"", category: "fun", tag: "anime"},
+    {name: "anilinks", url: "http://www.anilinks.neocities.org", description:"", category: "fun", tag: "anime"},
+  ];
+
+  export default bookmarkData;
