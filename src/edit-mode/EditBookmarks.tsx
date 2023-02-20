28c76c7e8edf6850d384c966c7fff52a35ff84cd
Misty Becker <mistyb01@uw.edu>
2023-02-19 20:37:07 -0800

    bookmarks are populated in editor

--- src/edit-mode/EditBookmarks.tsx
+++ src/edit-mode/EditBookmarks.tsx
@@ -9,16 +9,37 @@ interface Props {
 
 export const EditBookmarks: React.FC<Props> = (props) => {
 
-    const [selectedCategory, setSelectedCategory] = useState('');
-    
-  return (
-    <div className="edit-bookmarks-container">
+    const [selectedCategory, setSelectedCategory] = useState('all');
+
+    let filteredBookmarks = props.bookmarkData;
+    if (selectedCategory !== 'all') {
+        filteredBookmarks = props.bookmarkData.filter(bookmark => bookmark.category === selectedCategory);
+    }
+
+    return (
+    <div className="edit-bookmarks-container vertical-space">
         <Select 
             options={
                 props.categoryList.map(category => ({label: category, value: category}))}
             onChange={(opt) => setSelectedCategory(opt!.value)}
             placeholder={'filter by category'}
         />
+        <ul className="editor-list vertical-space">
+            <li className="editor-list__heading horizontal-space">
+                <span>name</span>
+                <span>url</span>
+                <span>category</span>
+                <span>tags</span>
+            </li>
+        {filteredBookmarks.map((bookmark) => 
+            <li className='horizontal-space'>
+                <span>{bookmark.name}</span>
+                <input type="text" value={bookmark.url}/>
+                <span>{bookmark.category}</span>
+                <span>{bookmark.tag}</span>
+            </li>
+        )}
+        </ul>
     </div>
   );
 }
