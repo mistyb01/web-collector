b5a9578f94cdec2f029326c09c0f3fe999efde82
Misty Becker <mistyb01@uw.edu>
2023-08-05 17:06:06 -0700

    skip to main content element

--- src/Bookmarks.tsx
+++ src/Bookmarks.tsx
@@ -1,36 +1,46 @@
-import React  from 'react';
-import { BookmarkType } from './@types/app';
-var uniqid = require('uniqid');
+import React from "react";
+import { BookmarkType } from "./@types/app";
+var uniqid = require("uniqid");
 
 interface Props {
-  bookmarkData: BookmarkType[],
-  category: string,
-  tag: string
+  bookmarkData: BookmarkType[];
+  category: string;
+  tag: string;
 }
 
 export const Bookmarks: React.FC<Props> = (props) => {
   let filteredBookmarks = props.bookmarkData;
-  if (props.category !== 'all') {
-   filteredBookmarks = props.bookmarkData.filter((bookmark) => bookmark.category === props.category);
+  if (props.category !== "all") {
+    filteredBookmarks = props.bookmarkData.filter(
+      (bookmark) => bookmark.category === props.category
+    );
   }
-  if (props.tag !== 'all') {
-    filteredBookmarks = filteredBookmarks.filter((bookmark) => bookmark.tag === props.tag)
+  if (props.tag !== "all") {
+    filteredBookmarks = filteredBookmarks.filter(
+      (bookmark) => bookmark.tag === props.tag
+    );
   }
 
   return (
     <div className="bookmarks-container">
       <ul className="bookmarks-list">
-        {filteredBookmarks.map((bookmark) => {
-            return (
-              <li key={uniqid()} className="bookmarks-list__item">
-                <a href={bookmark.url} target="_blank">{bookmark.name}</a>
-              </li>
-            );
-          }
-        )}
+        {filteredBookmarks.map((bookmark, i) => {
+          return (
+            <li
+              key={uniqid()}
+              className="bookmarks-list__item"
+              id={`bookmark-${i}`}
+              tabIndex={0}
+            >
+              <a href={bookmark.url} target="_blank">
+                {bookmark.name}
+              </a>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
-}
+};
 
 export default Bookmarks;
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,80 +1,104 @@
-import React, { useState }  from 'react';
-import Bookmarks from './Bookmarks';
-import Tags from './Tags';
-import Categories from './Categories';
-import { BookmarkType } from './@types/app';
-import { Link } from 'react-router-dom';
-import AnimatedPage from './AnimatedPage';
+import React, { useState } from "react";
+import Bookmarks from "./Bookmarks";
+import Tags from "./Tags";
+import Categories from "./Categories";
+import { BookmarkType } from "./@types/app";
+import { Link } from "react-router-dom";
+import AnimatedPage from "./AnimatedPage";
 
 interface Props {
-  bookmarkData: BookmarkType[],
+  bookmarkData: BookmarkType[];
 }
 
 export const Home: React.FC<Props> = (props) => {
   const [categoryIndex, setCategoryIndex] = useState(0);
-  const [tag, setTag] = useState('all');
-  
+  const [tag, setTag] = useState("all");
+
   //solution: category is no longer a piece of state that gets passed around.
-  // instead, the category index is state. the current category is then computed based off it. 
+  // instead, the category index is state. the current category is then computed based off it.
   function handleCategoryChange(e: React.MouseEvent<HTMLButtonElement>) {
-    let newIndex = categoryList.indexOf(e.currentTarget.id)
+    let newIndex = categoryList.indexOf(e.currentTarget.id);
     setCategoryIndex(newIndex);
-    setTag('all');
+    setTag("all");
   }
 
   function handleTagChange(e: React.MouseEvent<HTMLButtonElement>) {
     if (e.currentTarget.id === tag) {
-      setTag('all');
+      setTag("all");
     } else {
-    setTag(e.currentTarget.id);
+      setTag(e.currentTarget.id);
     }
   }
-  
-  const categoryList = Array.from(new Set(props.bookmarkData.map(bookmark => bookmark.category)));
+
+  const categoryList = Array.from(
+    new Set(props.bookmarkData.map((bookmark) => bookmark.category))
+  );
   const category = categoryList[categoryIndex];
 
-  let hasSavedData = localStorage.getItem('bookmarkData') !== null;
+  let hasSavedData = localStorage.getItem("bookmarkData") !== null;
 
   return (
-   <AnimatedPage>
-    <header className="vertical-space">
-          <h1 className="header-logo">web collector</h1>
-          <div className="header-buttons horizontal-space">
-            {
+    <AnimatedPage>
+      <header className="vertical-space">
+        <h1 className="header-logo">web collector</h1>
+        <Link to="#bookmark-0" className="skip-to-content">
+          skip to main content
+        </Link>
+        <div className="header-buttons horizontal-space">
+          {
             <>
-            <Link to='/add'><button className="header__button">add bookmark</button></Link>
-            {hasSavedData && <Link to='/edit'><button className="header__button">edit</button></Link>}
+              <Link to="/add">
+                <button className="header__button">add bookmark</button>
+              </Link>
+              {hasSavedData && (
+                <Link to="/edit">
+                  <button className="header__button">edit</button>
+                </Link>
+              )}
             </>
-            }
-          </div>
-    </header>
-    {hasSavedData ?
-    <main className="home-container">
-    <div className="filter-menu">
-        <h3 className="filter-menu__header">categories</h3>
-            <Categories 
-            categoryList={categoryList} 
-            category={category} 
-            handleCategoryChange={handleCategoryChange}/>
+          }
+        </div>
+      </header>
+      {hasSavedData ? (
+        <main className="home-container">
+          <div className="filter-menu">
+            <h3 className="filter-menu__header">categories</h3>
+            <Categories
+              categoryList={categoryList}
+              category={category}
+              handleCategoryChange={handleCategoryChange}
+            />
 
-        <h3 className="filter-menu__header">filters</h3>
-        <Tags 
-            bookmarkData={props.bookmarkData} 
-            category={category} 
-            tag={tag} 
-            handleTagChange={handleTagChange} />
-    </div>
-    <Bookmarks bookmarkData={props.bookmarkData} category={category} tag={tag}/>
-    </main> :
-    <div className="first-screen page-container vertical-space">
-      <h2>save your first bookmark!</h2>
-      <p>This page will show your bookmarks, <br/>along with categories and tags to sort them by.</p>
-      <div className="horizontal-space">
-        <Link to='/add'><button className="form-button">add bookmark</button></Link>
-      </div>
-    </div> }
-   </AnimatedPage>
+            <h3 className="filter-menu__header">filters</h3>
+            <Tags
+              bookmarkData={props.bookmarkData}
+              category={category}
+              tag={tag}
+              handleTagChange={handleTagChange}
+            />
+          </div>
+          <Bookmarks
+            bookmarkData={props.bookmarkData}
+            category={category}
+            tag={tag}
+          />
+        </main>
+      ) : (
+        <div className="first-screen page-container vertical-space">
+          <h2>save your first bookmark!</h2>
+          <p>
+            This page will show your bookmarks, <br />
+            along with categories and tags to sort them by.
+          </p>
+          <div className="horizontal-space">
+            <Link to="/add">
+              <button className="form-button">add bookmark</button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </AnimatedPage>
   );
-}
+};
 
 export default Home;
